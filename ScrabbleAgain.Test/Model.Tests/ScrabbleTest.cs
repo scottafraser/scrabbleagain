61df1f9cc5688ea3ecbb8b3ef4c82a5ed9b53a8a
Student <example@example.com>
2018-06-28 11:27:56 -0700

    dammit

---
+++ ScrabbleAgain.Test/Model.Tests/ScrabbleTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleAgain;
+
+namespace ScrabbleAgain.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetSetUserWords_GetsSetsUserWords_StringList()
+        {
+            Points TestPoints = new Points();
+            string testWord = "test";
+            TestPoints.SetCharList(testWord);
+            Assert.AreEqual(testWord, TestAnagram.GetCharList());
+        }
+    }
+}
---
+++ ScrabbleAgain/Models/Scrabble.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scrabble
+{
+
+    public class Points
+    {
+        private char _charList;
+        private int _intList;
+
+        public void SetCharList(char a)
+        {
+            _charList = a;
+        }
+
+        public char GetCharList()
+        {
+            return _charList;
+        }
+
+        public void SetIntList(int one)
+        {
+            _intList = one;
+        }
+
+        public int GetIntList()
+        {
+            return _intList;
+        }
+
+
+        public static Dictionary<char, int> charPoints = new Dictionary<char, int>() { { 'a', 1 }, { 'e', 1 }, { 'i', 1 }, { 'o', 1 }, { 'u', 1 }, { 'l', 1 }, { 'n', 1 }, { 'r', 1 }, { 's', 1 }, { 't', 1 }, { 'd', 2 }, { 'g', 2 }, { 'b', 3 }, { 'c', 3 }, { 'm', 3 }, { 'p', 3 }, { 'f', 4 }, { 'h', 4 }, { 'v', 4 }, { 'w', 4 }, { 'y', 4 }, { 'k', 5 }, { 'j', 8 }, { 'x', 8 }, { 'q', 10 }, { 'z', 10 } };
+
+        //public void UserWord();
+            //{
+                
+            //}
+
+
+
+
+            static void Main()
+            {
+                //nothing here yet
+            }
+        }
+    }
